8fc1dfc941b200cc9d73fa3a0b3845cb67c601a6
sando1con <147230274+sando1con@users.noreply.github.com>
2024-10-10 02:34:14 +0900

    server commit
    server commit

---
+++ src/test/java/ppurio/brothers/PpurioApplicationTests.java
@@ -0,0 +1,13 @@
+package ppurio.brothers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpurioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
