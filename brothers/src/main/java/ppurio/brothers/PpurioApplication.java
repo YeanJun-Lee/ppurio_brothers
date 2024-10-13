9ceba0d351004030f249db152df5b6cb6ec4d606
sando1con <147230274+sando1con@users.noreply.github.com>
2024-10-14 01:17:12 +0900

    server folder upload

---
+++ brothers/src/main/java/ppurio/brothers/PpurioApplication.java
@@ -0,0 +1,12 @@
+package ppurio.brothers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpurioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpurioApplication.class, args);
+	}
+}
